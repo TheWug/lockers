93aec54d95d5ef1ba3b88db3a714776f2ac56516
Ryan S <wuuug@telegram>
2021-06-15 20:01:49 -0700

    WHITESPACE FIX
    strip trailing whitespace from lines.
    If something creates a conflict with this commit,
    keep your version, and just correct the whitespace.

--- lockers_test.go
+++ lockers_test.go
@@ -19,7 +19,7 @@ func Test_SizeSpec_Contains(t *testing.T) {
 		"skewed": X{SizeSpec{10, 10, 10}, SizeSpec{9,  11, 10}, false, false},
 		"denormalized": X{SizeSpec{10, 11, 12}, SizeSpec{12, 11, 10}, false, false},
 	}
-	
+
 	for k, v := range tests {
 		t.Run(k, func(t *testing.T) {
 			if v.first.Contains(v.second) != v.forward {
@@ -37,7 +37,7 @@ func Test_SizeSpec_Volume(t *testing.T) {
 		value SizeSpec
 		answer int64
 	}
-	
+
 	tests := map[string]X{
 		"10x10x10": X{SizeSpec{10,10,10}, 10*10*10},
 		"10x10x11": X{SizeSpec{10,10,11}, 10*10*11},
@@ -45,7 +45,7 @@ func Test_SizeSpec_Volume(t *testing.T) {
 		"13x12x11": X{SizeSpec{13,12,11}, 13*12*11},
 		"negative": X{SizeSpec{-1,1,1}, -1},
 	}
-	
+
 	for k, v := range tests {
 		t.Run(k, func(t *testing.T) {
 			if v.value.Volume() != v.answer {
@@ -67,9 +67,9 @@ func Test_SizeSpec_Normalize(t *testing.T) {
 		"negate-2": SizeSpec{Length: 5, Width: -3, Height: 1},
 		"negate-3": SizeSpec{Length: 5, Width: 3, Height: -1},
 	}
-	
+
 	answer := SizeSpec{Length: 5, Width: 3, Height: 1}
-	
+
 	for k, v := range tests {
 		t.Run(k, func(t *testing.T) {
 			if v.Normalize() != answer {
@@ -87,7 +87,7 @@ func (mcs MockInventory) ControlSpec(size_id LockerSize) *LockerControlSpec {
 	if size_id == LockerSize(0) {
 		return mcs.CompareFrom
 	}
-	
+
 	return mcs.CompareTo
 }
 
@@ -96,14 +96,14 @@ func Test_LockerSize_Before(t *testing.T) {
 		cmp LockerControlSpec
 		expected bool
 	}
-	
+
 	inv := MockInventory{
 		CompareFrom: &LockerControlSpec{
 			VirtualCapacity: 50,
 			Size: SizeSpec{5, 5, 5},
 		},
 	}
-	
+
 	tests := map[string]X{
 		"75-6": X{LockerControlSpec{VirtualCapacity: 75, Size: SizeSpec{6,6,6}}, false},
 		"75-5": X{LockerControlSpec{VirtualCapacity: 75, Size: SizeSpec{5,5,5}}, false},
@@ -115,7 +115,7 @@ func Test_LockerSize_Before(t *testing.T) {
 		"25-5": X{LockerControlSpec{VirtualCapacity: 25, Size: SizeSpec{5,5,5}}, true},
 		"25-4": X{LockerControlSpec{VirtualCapacity: 25, Size: SizeSpec{4,4,4}}, true},
 	}
-	
+
 	for k, v := range tests {
 		t.Run(k, func(t *testing.T) {
 			inv.CompareTo = &v.cmp
@@ -131,7 +131,7 @@ func Test_LockerControlSpec_Full(t *testing.T) {
 	if !spec.Full() {
 		t.Errorf("%v not full, should have been", spec)
 	}
-	
+
 	spec.Lockers = append(spec.Lockers, 1)
 	if spec.Full() {
 		t.Errorf("%v full, should not have been", spec)
@@ -155,44 +155,44 @@ func Test_Inventory_ControlSpec(t *testing.T) {
 func Example_Locker_PutFetch() {
 	locker := Locker{
 	}
-	
+
 	pkg := Package{
 	}
-	
+
 	no_error := errors.New("No error")
-	
+
 	e := locker.Put(&pkg)
 	if e == nil { e = no_error }
 	fmt.Println(e.Error()) // no error
-	
+
 	p, e := locker.Fetch()
 	if e == nil { e = no_error }
 	fmt.Println(e.Error()) // no error
 	if p != &pkg {
 		fmt.Println("got a different package back?")
 	}
-	
+
 	locker = Locker{Contents: &pkg}
 	pkg = Package{}
-	
+
 	e = locker.Put(&pkg)
 	if e == nil { e = no_error }
 	fmt.Println(e.Error()) // locker is not empty
-	
+
 	locker = Locker{}
 	pkg = Package{StoredIn: &locker}
-	
+
 	e = locker.Put(&pkg)
 	if e == nil { e = no_error }
 	fmt.Println(e.Error()) // package already in locker
-	
+
 	locker = Locker{}
 	pkg = Package{}
-	
+
 	_, e = locker.Fetch()
 	if e == nil { e = no_error }
 	fmt.Println(e.Error()) // Tried to fetch from empty locker
-	
+
 	// Output:
 	// No error
 	// No error
@@ -206,7 +206,7 @@ func CompareControls(t *testing.T, a, b *LockerControlSpec, ia, ib *Inventory) b
 	if a.Size != b.Size {
 		return false
 	}
-	
+
 	// compare the biggerthan values by comparing the sizes they indirect against
 	if len(a.BiggerThan) != len(b.BiggerThan) {
 		return false
@@ -221,7 +221,7 @@ func CompareControls(t *testing.T, a, b *LockerControlSpec, ia, ib *Inventory) b
 	if len(sizes) != 0 {
 		return false
 	}
-	
+
 	// compare the smallerthan values the same way
 	if len(a.SmallerThan) != len(b.SmallerThan) {
 		return false
@@ -236,28 +236,28 @@ func CompareControls(t *testing.T, a, b *LockerControlSpec, ia, ib *Inventory) b
 	if len(sizes) != 0 {
 		return false
 	}
-	
+
 	// check the length of the lockers. the indices themselves may vary but there
 	// should be the same number of them
 	if len(a.Lockers) != len(b.Lockers) {
 		return false
 	}
-	
+
 	// check the virtual capacity
 	if a.VirtualCapacity != b.VirtualCapacity {
 		return false
 	}
-	
+
 	return true
 }
 
 func ValidateInventory(t *testing.T, a *Inventory) (bool, string) {
 	t.Helper()
-	
+
 	if len(a.Sizes) != len(a.Control) {
 		return false, "len(sizes) and len(control) mismatch"
 	}
-	
+
 	for k, v := range a.Control {
 		if k != v.SizeId { return false, "inconsistent ControlSpec.SizeId to key" }
 		if k != a.Sizes[v.Size] { return false, "inconsistent Sizes[ControlSpec.Size] to key" }
@@ -266,73 +266,73 @@ func ValidateInventory(t *testing.T, a *Inventory) (bool, string) {
 			if a.Lockers[i].SizeId != k { return false, "inconsistent locker SizeId to ControlSpec" }
 		}
 	}
-	
+
 	for k, v := range a.LockersById {
 		if v >= len(a.Lockers) { return false, "locker index in LockersById too big" }
 		if a.Lockers[v].Id != k { return false, "inconsistent locker id to LockersById" }
 	}
-	
+
 	return true, ""
 }
 
 func CompareInventories(t *testing.T, a, b *Inventory) (bool, string) {
 	t.Helper()
-	
+
 	// compare Sizes.
 	// the keys should be the same, but will be in a randomized order.
 	if len(a.Sizes) != len(b.Sizes) {
 		return false, ".Sizes lengths not equal"
 	}
-	
+
 	keys := make(map[SizeSpec]bool)
-	
+
 	for k, _ := range a.Sizes {
 		keys[k] = true
 	}
-	
+
 	for k, _ := range b.Sizes {
 		delete(keys, k)
 	}
-	
+
 	if len(keys) != 0 {
 		return false, ".Sizes keys/values mismatched"
 	}
-	
-	// compare the control structures. SizeIds may be different between runs, 
+
+	// compare the control structures. SizeIds may be different between runs,
 	// and thusly they may occur out of order, but we can translate the SizeId
 	// using .Sizes
 	if len(a.Control) != len(b.Control) {
 		return false, ".Control lengths not equal"
 	}
-	
+
 	for size_id, control := range a.Control {
 		remote_size_id, ok := b.Sizes[control.Size]
 		if !ok { return false, fmt.Sprintf("missing control size %v", size_id) }
 		remote_control, ok := b.Control[remote_size_id]
 		if !ok { return false, fmt.Sprintf("missing control for size %v", size_id) }
-		
+
 		if !CompareControls(t, control, remote_control, a, b) {
 			return false, fmt.Sprintf("Control values not equal:\n%+v\n%+v", control, remote_control)
 		}
 	}
-	
-	
+
+
 	// compare the lockers. they can be out of order, so this is challenging.
 	// so instead, create a map of flattened proxy objects and compare those.
 	if len(a.Lockers) != len(b.Lockers) {
 		return false, ".Lockers lengths not equal"
 	}
-	
+
 	type PackageProxy struct {
 		Size SizeSpec
 		Id string
 	}
-	
+
 	type LockerProxy struct {
 		Contents PackageProxy
 		Size SizeSpec
 	}
-	
+
 	locker_proxies := make(map[LockerProxy]int)
 	for _, x := range a.Lockers {
 		p := LockerProxy{
@@ -344,7 +344,7 @@ func CompareInventories(t *testing.T, a, b *Inventory) (bool, string) {
 				Size: x.Contents.Size,
 			}
 		}
-		
+
 		locker_proxies[p] = locker_proxies[p] + 1
 	}
 	for _, x := range b.Lockers {
@@ -357,7 +357,7 @@ func CompareInventories(t *testing.T, a, b *Inventory) (bool, string) {
 				Size: x.Contents.Size,
 			}
 		}
-		
+
 		locker_proxies[p] = locker_proxies[p] - 1
 	}
 	for k, v := range locker_proxies {
@@ -365,27 +365,27 @@ func CompareInventories(t *testing.T, a, b *Inventory) (bool, string) {
 			return false, fmt.Sprintf("Incorrect count for locker proxy: %+v", k)
 		}
 	}
-	
+
 	// compare lockers by id. the keys are randomly generated, and in general the indices may occur
 	// out of order, but since this map has all of the indices and the order doesn't matter, they
 	// should at least all be there.
 	if len(a.LockersById) != len(b.LockersById) {
 		return false, ".LockersById lengths not equal"
 	}
-	
+
 	locker_indices := make(map[int]bool)
-	
+
 	for _, v := range a.LockersById {
 		locker_indices[v] = true
 	}
 	for _, v := range b.LockersById {
 		delete(locker_indices, v)
 	}
-	
+
 	if len(locker_indices) != 0 {
 		return false, "leftover locker indices"
 	}
-	
+
 	return true, ""
 }
 
@@ -398,7 +398,7 @@ func Test_New_Inventory(t *testing.T) {
 		size_counts map[SizeSpec]int
 		result *Inventory
 	}
-	
+
 	tests := map[string]X {
 		"no-lockers": X{map[SizeSpec]int{}, &Inventory{
 			Lockers: make([]Locker, 0),
@@ -506,7 +506,7 @@ func Test_New_Inventory(t *testing.T) {
 			LockersByPackageId: make(map[string]int),
 		}},
 	}
-	
+
 	for k, v := range tests {
 		t.Run(k, func(t *testing.T) {
 			i := NewInventory(v.size_counts)
@@ -524,7 +524,7 @@ func Test_New_Inventory(t *testing.T) {
 
 func basic(t *testing.T) *Inventory {
 	t.Helper()
-	
+
 	return &Inventory{
 		Lockers: []Locker{
 			Locker{"1", 100, nil},
@@ -590,7 +590,7 @@ func basic(t *testing.T) *Inventory {
 
 func cplx(t *testing.T) *Inventory {
 	t.Helper()
-	
+
 	return &Inventory{
 		Lockers: []Locker{
 			Locker{"1", 100, nil},
@@ -658,12 +658,12 @@ func cplx(t *testing.T) *Inventory {
 
 func Test_Inventory_AllocateLocker(t *testing.T) {
 	inv := basic(t)
-	
+
 	type X struct {
 		size SizeSpec
 		panics bool
 	}
-	
+
 	tests := map[string]X{
 		"smallest": X{SizeSpec{1,1,1}, false},
 		"medium": X{SizeSpec{2,2,2}, false},
@@ -671,7 +671,7 @@ func Test_Inventory_AllocateLocker(t *testing.T) {
 		"overallocate": X{SizeSpec{4,4,4}, true},
 		"missing": X{SizeSpec{5,5,5}, true},
 	}
-	
+
 	for k, v := range tests {
 		t.Run(k, func(t *testing.T) {
 			defer func(){
@@ -682,11 +682,11 @@ func Test_Inventory_AllocateLocker(t *testing.T) {
 					t.Errorf("Expected normal completion, but panic'd")
 				}
 			}()
-			
+
 			size := inv.Sizes[v.size]
 			var inner_available []int
 			var capacity int
-			
+
 			if x, ok := inv.Control[size]; ok {
 				inner_available = x.Lockers
 				capacity = x.VirtualCapacity
@@ -695,24 +695,24 @@ func Test_Inventory_AllocateLocker(t *testing.T) {
 			for _, x := range inner_available {
 				available[x] = true
 			}
-			
+
 			// a should be removed from inv.Control[size].Lockers
 			// if the slice has no elements, or if size isn't in Control, it will panic
 			a := inv.AllocateLocker(size)
-			
+
 			if len(available) != len(inv.Control[size].Lockers) + 1 {
 				t.Error("Mismatched lengths between expected and actual lockers free")
 			}
-			
+
 			delete(available, a)
 			for _, x := range inv.Control[size].Lockers {
 				delete(available, x)
 			}
-			
+
 			if len(available) != 0 {
 				t.Error("Mismatched values in expected and actual lockers free")
 			}
-			
+
 			if capacity == inv.Control[size].VirtualCapacity {
 				t.Error("Virtual capacity unchanged")
 			}
@@ -725,26 +725,26 @@ func Test_Inventory_AdjustVirtualCapacity(t *testing.T) {
 	inv1.Control[LockerSize(100)].VirtualCapacity += 1
 	inv2.Control[LockerSize(100)].VirtualCapacity += 2
 	inv3.Control[LockerSize(100)].VirtualCapacity += 3
-	
+
 	inv2.Control[LockerSize(200)].VirtualCapacity += 2
 	inv3.Control[LockerSize(200)].VirtualCapacity += 3
-	
+
 	inv3.Control[LockerSize(300)].VirtualCapacity += 3
-	
+
 	type X struct {
 		add int
 		addto LockerSize
 		result *Inventory
 		panics bool
 	}
-	
+
 	tests := map[string]X{
 		"smallest": X{1, 100, inv1, false},
 		"medium": X{2, 200, inv2, false},
 		"largest": X{3, 300, inv3, false},
 		"missing": X{4, 400, nil, true},
 	}
-	
+
 	for k, v := range tests {
 		t.Run(k, func(t *testing.T) {
 			defer func() {
@@ -755,10 +755,10 @@ func Test_Inventory_AdjustVirtualCapacity(t *testing.T) {
 					t.Errorf("Expected normal completion, but panic'd")
 				}
 			}()
-			
+
 			inv := basic(t)
 			inv.AdjustVirtualCapacity(v.addto, v.add)
-			
+
 			eq, explain := CompareInventories(t, inv, v.result)
 			if !eq {
 				t.Errorf("Incorrect NewInventory output:\n%+v\nExpected:\n%+v\n%s", inv, v.result, explain)
@@ -769,29 +769,29 @@ func Test_Inventory_AdjustVirtualCapacity(t *testing.T) {
 
 func Test_Inventory_DeallocateLocker(t *testing.T) {
 	inv := basic(t)
-	
+
 	control, ok := inv.Control[400]
 	if !ok || len(control.Lockers) != 0 {
 		t.Error("precondition failed")
 	}
-	
+
 	capacity := control.VirtualCapacity
-	
+
 	inv.DeallocateLocker(7)
 	inv.DeallocateLocker(6)
-	
+
 	available := make(map[int]bool)
 	for _, x := range control.Lockers {
 		available[x] = true
 	}
-	
+
 	delete(available, 6)
 	delete(available, 7)
 
 	if len(control.Lockers) != 2 || len(available) != 0 {
 		t.Error("Element mismatch when checking available list")
 	}
-	
+
 	if capacity == control.VirtualCapacity {
 		t.Errorf("Virtual capacity unchanged: %d %d", capacity, control.VirtualCapacity)
 	}
@@ -800,11 +800,11 @@ func Test_Inventory_DeallocateLocker(t *testing.T) {
 func Test_Inventory_GetMostSuitableLockerSize(t *testing.T) {
 	inv1, inv2, inv3, inv4 := cplx(t), cplx(t), cplx(t), cplx(t)
 	// inv1 is normal and unmodified
-	
+
 	// inv2 has all {1,1,1} lockers allocated
 	inv2.Control[100].VirtualCapacity -= len(inv2.Control[200].Lockers)
 	inv2.Control[100].Lockers = nil
-	
+
 	// inv3 has all {1,1,1} lockers allocated, and one {4,1,1} locker resized to {3,3,1}
 	inv3.Control[100].VirtualCapacity -= len(inv2.Control[200].Lockers)
 	inv3.Control[100].Lockers = nil
@@ -813,7 +813,7 @@ func Test_Inventory_GetMostSuitableLockerSize(t *testing.T) {
 	inv3.Lockers[4].SizeId = 300
 	inv3.Control[200].VirtualCapacity -= 1
 	inv3.Control[300].VirtualCapacity += 1
-	
+
 	// inv4 has no available lockers except for small
 	for _, c := range inv4.Control {
 		if (c.Size == SizeSpec{1,1,1}) {
@@ -823,14 +823,14 @@ func Test_Inventory_GetMostSuitableLockerSize(t *testing.T) {
 		c.Lockers = nil
 		c.VirtualCapacity = 0
 	}
-	
+
 	type X struct {
 		inv *Inventory
 		size SizeSpec
 		answer SizeSpec
 		is_error bool
 	}
-	
+
 	tests := map[string]X{
 		"normal-micro":     X{inv1, SizeSpec{1,0,0}, SizeSpec{1,1,1}, false},
 		"normal-small":     X{inv1, SizeSpec{1,1,1}, SizeSpec{1,1,1}, false},
@@ -839,7 +839,7 @@ func Test_Inventory_GetMostSuitableLockerSize(t *testing.T) {
 		"normal-med2":      X{inv1, SizeSpec{2,2,1}, SizeSpec{3,3,1}, false},
 		"normal-big-ambig": X{inv1, SizeSpec{4,4,2}, SizeSpec{5,5,5}, false},
 		"normal-toobig":    X{inv1, SizeSpec{7,1,1}, SizeSpec{0,0,0}, true},
-		
+
 		"nosmall-micro":     X{inv2, SizeSpec{1,0,0}, SizeSpec{5,1,1}, false},
 		"nosmall-small":     X{inv2, SizeSpec{1,1,1}, SizeSpec{5,1,1}, false},
 		"nosmall-med-ambig": X{inv2, SizeSpec{3,1,1}, SizeSpec{5,1,1}, false},
@@ -847,7 +847,7 @@ func Test_Inventory_GetMostSuitableLockerSize(t *testing.T) {
 		"nosmall-med2":      X{inv2, SizeSpec{2,2,1}, SizeSpec{3,3,1}, false},
 		"nosmall-big-ambig": X{inv2, SizeSpec{4,4,2}, SizeSpec{5,5,5}, false},
 		"nosmall-toobig":    X{inv2, SizeSpec{7,1,1}, SizeSpec{0,0,0}, true},
-		
+
 		"nosmall-swapped-micro":     X{inv3, SizeSpec{1,0,0}, SizeSpec{3,3,1}, false},
 		"nosmall-swapped-small":     X{inv3, SizeSpec{1,1,1}, SizeSpec{3,3,1}, false},
 		"nosmall-swapped-med-ambig": X{inv3, SizeSpec{3,1,1}, SizeSpec{3,3,1}, false},
@@ -855,7 +855,7 @@ func Test_Inventory_GetMostSuitableLockerSize(t *testing.T) {
 		"nosmall-swapped-med2":      X{inv3, SizeSpec{2,2,1}, SizeSpec{3,3,1}, false},
 		"nosmall-swapped-big-ambig": X{inv3, SizeSpec{4,4,2}, SizeSpec{5,5,5}, false},
 		"nosmall-swapped-toobig":    X{inv3, SizeSpec{7,1,1}, SizeSpec{0,0,0}, true},
-		
+
 		"empty-micro":     X{inv4, SizeSpec{1,0,0}, SizeSpec{1,1,1}, false},
 		"empty-small":     X{inv4, SizeSpec{1,1,1}, SizeSpec{1,1,1}, false},
 		"empty-med-ambig": X{inv4, SizeSpec{3,1,1}, SizeSpec{0,0,0}, true},
@@ -864,7 +864,7 @@ func Test_Inventory_GetMostSuitableLockerSize(t *testing.T) {
 		"empty-big-ambig": X{inv4, SizeSpec{4,4,2}, SizeSpec{0,0,0}, true},
 		"empty-toobig":    X{inv4, SizeSpec{7,1,1}, SizeSpec{0,0,0}, true},
 	}
-	
+
 	for k, v := range tests {
 		t.Run(k, func(t *testing.T) {
 			out, err := v.inv.GetMostSuitableLockerSize(v.size)
@@ -878,12 +878,12 @@ func Test_Inventory_GetMostSuitableLockerSize(t *testing.T) {
 				t.Errorf("Expected error, but got %v instead", out)
 				return
 			}
-			
+
 			if out != v.inv.Sizes[v.answer] {
 				t.Errorf("Wrong answer: expected %v, got %v", v.answer, v.inv.Control[out].Size)
 			}
 		})
-	}	
+	}
 }
 
 func Test_Inventory_DepositPackage(t *testing.T) {
@@ -892,10 +892,10 @@ func Test_Inventory_DepositPackage(t *testing.T) {
 		pkg *Package
 		is_error bool
 	}
-	
+
 	inv := cplx(t)
 	inv.LockersByPackageId["dupe"] = 0
-	
+
 	tests := map[string]X{
 		"tiny":        X{cplx(t), &Package{"a", SizeSpec{1,0,0}, nil}, false},
 		"small":       X{cplx(t), &Package{"b", SizeSpec{1,1,1}, nil}, false},
@@ -908,7 +908,7 @@ func Test_Inventory_DepositPackage(t *testing.T) {
 		"stored-pkg":  X{inv,     &Package{"h", SizeSpec{1,1,1}, &inv.Lockers[0]}, true},
 		"no-space":    X{&Inventory{}, &Package{"h", SizeSpec{1,1,1}, nil}, true},
 	}
-	
+
 	for k, v := range tests {
 		t.Run(k, func(t *testing.T) {
 			id, err := v.inv.DepositPackage(v.pkg)
@@ -920,15 +920,15 @@ func Test_Inventory_DepositPackage(t *testing.T) {
 			} else if v.is_error {
 				t.Error("Expected error, but completed successfully")
 			}
-			
+
 			if v.pkg.StoredIn == nil {
 				t.Error("Package was not stored in a locker")
 			}
-			
+
 			if v.pkg.StoredIn.Contents != v.pkg {
 				t.Error("Package <--> Locker mapping inconsistent")
 			}
-			
+
 			if &v.inv.Lockers[v.inv.LockersById[id]] != v.pkg.StoredIn {
 				t.Error("Bad return value: non-mapping ID")
 			}
@@ -940,7 +940,7 @@ func cplx_pkg(t *testing.T) (*Inventory, *Package) {
 	t.Helper()
 
 	inv := cplx(t)
-	
+
 	ctrl := inv.Control[200]
 	locker_index := ctrl.Lockers[len(ctrl.Lockers) - 1]
 	ctrl.Lockers = ctrl.Lockers[:len(ctrl.Lockers) - 1]
@@ -955,7 +955,7 @@ func cplx_pkg(t *testing.T) (*Inventory, *Package) {
 	locker.Id = "locker"
 	inv.LockersById[locker.Id] = locker_index
 	inv.LockersByPackageId[pkg.Id] = locker_index
-	
+
 	return inv, pkg
 }
 
@@ -967,19 +967,19 @@ func Test_Inventory_RetrievePackage(t *testing.T) {
 		package_id *string
 		is_error bool
 	}
-	
+
 	tests := map[string]X{
 		"normal":  X{nil, false},
 		"missing": X{sp("no-id"), true},
 	}
-	
+
 	for k, v := range tests {
 		t.Run(k, func(t *testing.T) {
 			inv, pkg := cplx_pkg(t)
 			if v.package_id != nil {
 				pkg.Id = *v.package_id
 			}
-			
+
 			output, err := inv.RetrievePackage(pkg)
 			if err != nil && v.is_error {
 				return
@@ -990,7 +990,7 @@ func Test_Inventory_RetrievePackage(t *testing.T) {
 				t.Error("Unespected success")
 				return
 			}
-			
+
 			if output != pkg {
 				t.Errorf("Got unexpected package back")
 			}
@@ -1004,21 +1004,21 @@ func Test_Inventory_RetrievePackageByLockerId(t *testing.T) {
 		locker_id *string
 		is_error bool
 	}
-	
+
 	tests := map[string]X{
 		"normal":  X{nil, false},
 		"missing": X{sp("8"), true},
 	}
-	
+
 	for k, v := range tests {
 		t.Run(k, func(t *testing.T) {
 			inv, pkg := cplx_pkg(t)
 			locker_id := pkg.StoredIn.Id
-			
+
 			if v.locker_id != nil {
 				locker_id = *v.locker_id
 			}
-			
+
 			output, err := inv.RetrievePackageByLockerId(locker_id)
 			if err != nil && v.is_error {
 				return
@@ -1029,7 +1029,7 @@ func Test_Inventory_RetrievePackageByLockerId(t *testing.T) {
 				t.Error("Unespected success")
 				return
 			}
-			
+
 			if output != pkg {
 				t.Errorf("Got unexpected package back")
 			}
